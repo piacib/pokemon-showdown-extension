ce247e6501ff313c10d00d901fe95058103997df
piacib <ben.piacitelli@gmail.com>
2021-09-03 14:45:05 -0700

    moved error screen if app is not opened on pokemonshowdown to seperate component

---
+++ src/NotPokemonShowdownErrorScreen.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+export const NotPokemonShowdownErrorScreen = () => {
+  return (
+    <div>
+      <h1>
+        This Extension only works on <br />
+        <a href="https://play.pokemonshowdown.com/">Pokemon Showdown</a>
+      </h1>
+    </div>
+  );
+};
