41d9cd08a3cd62c9d5594210e935d32a9e378129
piacib <ben.piacitelli@gmail.com>
2021-09-06 08:39:04 -0700

    added function to check if user is in random bat

--- src/functions.ts
+++ src/functions.ts
@@ -1,5 +1,34 @@
+import { isRandomBattleReturn } from "./types";
 export const isDevelopmentMode = process.env.NODE_ENV === "development";
+const randomDataBattleTypes = [
+  "gen8randombattle",
+  "gen8randomdoublesbattle",
+  "gen8randombattlenodmax",
+  "gen7randombattle",
+  "gen7randomdoublesbattle",
+  "gen7letsgorandombattle",
+  "gen6randombattle",
+  "gen5randombattle",
+  "gen4randombattle",
+  "gen3randombattle",
+  "gen2randombattle",
+  "gen1randombattle",
+];
+
 export const getBattleType = (url: string) => {
   const battleType = url.match(/(?<=-).+?(?=-)/g);
-  return battleType ? battleType[0] : "";
+  if (!battleType) {
+    return "No Battle Type Found";
+  }
+  return randomDataBattleTypes.includes(battleType[0])
+    ? battleType[0]
+    : "Not a random battle";
+};
+
+export const isRandomBattle = (url: string): isRandomBattleReturn => {
+  const battleType = url.match(/(?<=-).+?(?=-)/g);
+  if (!battleType) {
+    return null;
+  }
+  return randomDataBattleTypes.includes(battleType[0]) ? battleType[0] : false;
 };
