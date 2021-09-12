f0423222802c2252e2b121a09fb83edaf8757521
piacib <ben.piacitelli@gmail.com>
2021-09-12 09:32:30 -0700

    removed unused hooks

--- src/hooks/useCountRenders.js
+++
@@ -1,6 +0,0 @@
-import { useRef } from 'react';
-// logs number of renders 
-export const useCountRenders = (text ='') => {
-    const renders = useRef(0);
-    console.log(`${text} renders `, renders.current++)
-}
