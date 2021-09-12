ab6e63cad7480bcb26db2606468b6de137320ed8
piacib <ben.piacitelli@gmail.com>
2021-09-12 12:32:24 -0700

    removed <br/> from file

--- src/components/NotPokemonShowdownErrorScreen.tsx
+++ src/components/NotPokemonShowdownErrorScreen.tsx
@@ -4,7 +4,7 @@ export const NotPokemonShowdownErrorScreen = () => {
   return (
     <div>
       <h1>
-        This Extension only works on <br />
+        This Extension only works on
         <a href="https://play.pokemonshowdown.com/">Pokemon Showdown</a>
       </h1>
     </div>
