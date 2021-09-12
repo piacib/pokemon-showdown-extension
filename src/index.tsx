33cb2fd1564ea8d029b4d64b144051ae9bcd5620
piacib <ben.piacitelli@gmail.com>
2021-09-12 11:55:32 -0700

    Added globalstyle

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { GlobalStyle } from "./styles/GlobalStyles";
 // import AppDesign from "./AppDesign";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
