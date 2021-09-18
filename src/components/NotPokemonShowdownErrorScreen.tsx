45ffd71d7806ef7c122fe5f6418bb7be971563a6
piacib <ben.piacitelli@gmail.com>
2021-09-18 10:21:41 -0700

    moved text into seperate h1 tag

--- src/components/NotPokemonShowdownErrorScreen.tsx
+++ src/components/NotPokemonShowdownErrorScreen.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export const NotPokemonShowdownErrorScreen = () => {
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
+      <h1>This Extension only works on </h1>
       <h1>
-        This Extension only works on
         <a href="https://play.pokemonshowdown.com/">Pokemon Showdown</a>
       </h1>
     </div>
