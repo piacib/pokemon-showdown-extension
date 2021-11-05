062c7380f3df3d2039b884744090ed8b48ef6ec1
Ben Piacitelli <ben.piacitelli@gmail.com>
2021-11-05 10:01:03 -0700

    added loading screen component

---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import LoadingSVG from '../media/loading.svg'
+export const LoadingScreen = () => {
+    return (
+            <img src={LoadingSVG} alt='Loading'/>
+    )
+}
