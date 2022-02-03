2b1807e98976008ac33fdf61b2e809202aa605ff
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-02-03 11:32:29 -1000

    created damgedisplay folder

---
+++ src/components/DamageDisplay/DamageDisplay.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { DamageDisplay } from './DamageDisplay'
+
+test('renders learn react link', () => {
+  render(<DamageDisplay typesArray={['water','rock']} />);
+
+  
+  
+});
