1993f056ff7b5abac5ab419aed2e9f96afc2e0f5
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-02-02 14:14:10 -1000

    centered text

--- src/components/NotInBattleErrorScreen.tsx
+++ src/components/NotInBattleErrorScreen.tsx
@@ -1,5 +1,15 @@
 import React from "react";
-
+import styled from "styled-components";
+const NotInBattleMessage = styled.h1`
+  text-align: center;
+  margin: auto 0;
+  height: 100px;
+  grid-row: 1/-1;
+`;
 export const NotInBattleErrorScreen = () => {
-  return <div>Start a battle to use the extension!</div>;
+  return (
+    <NotInBattleMessage>
+      Start a Battle to use the Extension!
+    </NotInBattleMessage>
+  );
 };
