5dc639983b3d508c216cee7fe856c06cf6c1e474
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-02-07 07:19:27 -1000

    renamed from DamageDisplayStyles

---
+++ src/components/DamageDisplay/DamageDisplay.styles.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import { typeColorConverter } from "../PokemonDataDisplay/pokemonTypeColorConverter";
+import { TypeColorInterface } from "../../types";
+
+export const DamageContainer = styled.div`
+  grid-row: 4;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  font-size: 0.95rem;
+`;
+export const DamageGroupContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: space-around;
+`;
+export const TypeBox = styled.div<TypeColorInterface>`
+  background-color: ${(props) => typeColorConverter[props.background]};
+  margin: 5px;
+  padding: 2px 5px;
+  border-radius: 10px;
+`;
+export const TypeBoxContainer = styled.div`
+  height: 100%;
+  width: 280px;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+`;
+export const DamageMultiplier = styled.p`
+  height: 100%;
+  vertical-align: middle;
+`;
