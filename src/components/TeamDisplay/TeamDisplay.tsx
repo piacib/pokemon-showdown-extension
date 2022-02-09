4f5766485c88258f83771bdd14d23a21ff1e3d5e
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-02-08 14:00:47 -1000

    made toggle so that display showsactive pokemon or selected

--- src/components/TeamDisplay/TeamDisplay.tsx
+++ src/components/TeamDisplay/TeamDisplay.tsx
@@ -5,21 +5,31 @@ import { ActivePokemon, TeamProps } from '../../types';
 import { OpponentsTeamUnavailable } from '../ErrorScreens/OpponentsTeamUnavailable';
 import { PokemonUnavailable } from '../ErrorScreens/PokemonUnavailable';
 import SpriteImage from '../SpriteImage';
-import {
-  getCurrentPokemon,
-  pokemonNameFilter,
-  getPokemonName,
-  testTeam,
-  testResults,
-} from './TeamDisplay.functions';
+import { getCurrentPokemon, pokemonNameFilter, getPokemonName } from './TeamDisplay.functions';
 //fetches latest pokemon data from auto updating dataset
 export const TeamDisplay = ({ team, isRandomBattle }: TeamProps) => {
-  const [currentPokemon, setCurrentPokemon] = useState<ActivePokemon>(getCurrentPokemon(team));
-  const testFilter = testTeam.map((x) => pokemonNameFilter(x));
-  console.log(testFilter.filter((x, idx) => x !== testResults[idx]));
+  const [activePokemon, setActivePokemon] = useState<ActivePokemon>(getCurrentPokemon(team));
+  const [selectedPokemon, setSelectedPokemon] = useState<string | null>(null);
+  const [displayedPokemon, setDisplayedPokemon] = useState<string | null>(null);
   useEffect(() => {
-    setCurrentPokemon(getCurrentPokemon(team));
+    setActivePokemon(getCurrentPokemon(team));
   }, [team]);
+  // swaps displayed pokemon between selected and active
+  // such that when active is displayed it will automatically
+  // chaange if the active pokemon is changed but continues
+  // displaying the selected pokemon is user selects a
+  // different pokemon
+  useEffect(() => {
+    if (selectedPokemon === null) {
+      setDisplayedPokemon(activePokemon?.pokemon1);
+    } else if (selectedPokemon === activePokemon?.pokemon1) {
+      setSelectedPokemon(null);
+    } else {
+      setDisplayedPokemon(selectedPokemon);
+    }
+  }, [selectedPokemon, activePokemon]);
+  console.log('activePokemon', activePokemon?.pokemon1);
+  console.log('selectedPokemon', selectedPokemon);
   return !team ? (
     <>
       <ButtonDisplay>
@@ -39,7 +49,7 @@ export const TeamDisplay = ({ team, isRandomBattle }: TeamProps) => {
           <Button
             key={pokemonNameFilter(x) + idx}
             onClick={() => {
-              setCurrentPokemon(getPokemonName(x));
+              setSelectedPokemon(getPokemonName(x).pokemon1);
             }}
           >
             <SpriteImage name={pokemonNameFilter(x)} />
@@ -47,11 +57,8 @@ export const TeamDisplay = ({ team, isRandomBattle }: TeamProps) => {
         ))}
       </ButtonDisplay>
 
-      {currentPokemon.pokemon1 ? (
-        <OpponentPokemonDataDisplay
-          pokemon={currentPokemon.pokemon1}
-          isRandomBattle={isRandomBattle}
-        />
+      {displayedPokemon ? (
+        <OpponentPokemonDataDisplay pokemon={displayedPokemon} isRandomBattle={isRandomBattle} />
       ) : (
         <PokemonUnavailable />
       )}
