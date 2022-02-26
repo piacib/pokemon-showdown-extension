c5c8a277f9abe5ae29efdeb0712d8d50dc0d6bc0
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-02-26 08:38:41 -1000

    moved text below pokemon images

--- src/components/ErrorScreens/PokemonUnavailable.tsx
+++ src/components/ErrorScreens/PokemonUnavailable.tsx
@@ -1,5 +1,8 @@
-import React from "react";
-
+import React from 'react';
+import styled from 'styled-components';
+const PokemonUnavailableContainer = styled.div`
+  grid-row: 3 / end;
+`;
 export const PokemonUnavailable = () => {
-  return <div>Pokemon Unavailable</div>;
+  return <PokemonUnavailableContainer>Pokemon Unavailable</PokemonUnavailableContainer>;
 };
