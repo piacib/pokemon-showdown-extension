37780786ad209f257dbdce2b4eeb17ca6ba36e29
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-02-28 17:22:48 -1000

    removed types

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,6 @@ export enum Sender {
   React,
   Content,
 }
-
 export interface ChromeMessage {
   from: Sender;
   message: any;
@@ -11,34 +10,7 @@ export interface PokemonResponse {
   opponentsTeam: string[] | null;
   usersTeam: string[] | null;
 }
-export type RandbatsPokemonData = {
-  [key: string]: {
-    level: Number;
-    abilities: string[];
-    items: string[];
-    moves: string[];
-  };
-};
-export interface Stats {
-  hp: number;
-  atk: number;
-  def: number;
-  spa: number;
-  spd: number;
-  spe: number;
-}
-export interface OtherFormatsDisplayProps {
-  pokemon: string;
-}
 export type isRandomBattleReturn = null | false | string;
-export interface ActivePokemon {
-  pokemon1: string | null;
-  pokemon2: string | null;
-}
-export interface TeamProps {
-  team: string[] | null;
-  isRandomBattle: isRandomBattleReturn;
-}
 export interface TypeColorInterface {
   background: string;
 }
