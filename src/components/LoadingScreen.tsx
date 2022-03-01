fdec648049ccf66608d85c1471b755115ed5112a
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-02-28 16:52:30 -1000

    added styles and pokeball img to loading screen

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
-import LoadingSVG from '../media/loading.svg';
-import styled from 'styled-components';
-const LoadingIcon = styled.img``;
+// import LoadingSVG from '../media/loading.svg';
+import LoadingSVG from '../media/pokeball.svg';
+import styled, { keyframes } from 'styled-components';
+const spinning = keyframes`
+    from {
+        transform: rotate(359deg);
+    }
+    to {
+        transform: rotate(0deg);
+    }
+  
+  `;
+const LoadingIcon = styled.img`
+  animation: ${spinning} 4s infinite linear;
+  height: 9rem;
+  width: 9rem;
+  grid-column: -1/ 4;
+  margin: auto;
+`;
 export const LoadingScreen = () => {
   return <LoadingIcon src={LoadingSVG} alt="Loading" />;
 };
