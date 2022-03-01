bd151ce2ec806c253e07edb7a439cbd0d9fc94e7
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-02-28 15:42:50 -1000

    formatting

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { GlobalStyle } from "./GlobalStyles";
-import reportWebVitals from "./reportWebVitals";
-import { theme } from "./theme";
-import { ThemeProvider } from "styled-components";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { GlobalStyle } from './GlobalStyles';
+import reportWebVitals from './reportWebVitals';
+import { theme } from './theme';
+import { ThemeProvider } from 'styled-components';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle theme={theme}/>
-<ThemeProvider theme={theme}>
-    <App />
-</ThemeProvider>
+    <GlobalStyle theme={theme} />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
