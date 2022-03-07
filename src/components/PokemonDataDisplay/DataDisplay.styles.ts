9e63afb8cec042865955a52f6772efcca3144a39
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-03-06 19:38:44 -1000

    moved styles to seperate files

--- src/components/PokemonDataDisplay/DataDisplay.styles.ts
+++ src/components/PokemonDataDisplay/DataDisplay.styles.ts
@@ -1,6 +1,4 @@
 import styled from 'styled-components';
-import { typeColorConverter } from './pokemonTypeColorConverter';
-import { TypeColorInterface } from '../../types';
 export const PropertyDisplay = styled.div`
   height: fit-content;
   display: flex;
@@ -8,9 +6,6 @@ export const PropertyDisplay = styled.div`
   padding-bottom: 10px;
   flex-wrap: wrap;
   align-items: center;
-`;
-const TypeColoredComponent = styled.div<TypeColorInterface>`
-  background-color: ${(props) => props.theme.color.typeColors[props.background]};
 `;
 export const HiddenPropertyText = styled.div`
   display: none;
@@ -41,40 +36,6 @@ export const PropertyBtn = styled(HoverDisplay)`
     font-size: inherit;
   }
 `;
-export const MoveBtn = styled(HoverDisplay)`
-  &:hover ${HiddenPropertyText} {
-    bottom: 30px;
-    right: 0;
-    min-width: 170px;
-    padding: 5px;
-    font-size: 1.1rem;
-  }
-`;
-export const MoveData = styled.ul`
-  list-style: none;
-  width: 100%;
-  padding: 0;
-`;
-export const MoveProperty = styled.li`
-  /* width: fit-content; */
-  white-space: nowrap;
-`;
-export const MoveDescription = styled(MoveProperty)`
-  white-space: initial;
-`;
-export const MovesDisplay = styled(PropertyDisplay)`
-  display: flex;
-`;
-export const MoveType = styled(MoveProperty)<TypeColorInterface>`
-  background-color: ${(props) => typeColorConverter[props.background]};
-  padding: 2px 6px;
-  width: fit-content;
-  margin: 0 auto;
-`;
-
-export const AbilitiesDisplay = styled(PropertyDisplay)``;
-export const ItemsDisplay = styled(PropertyDisplay)``;
-
 export const PokemonName = styled.a`
   justify-self: start;
   grid-row: 3;
@@ -90,42 +51,3 @@ export const PropertiesContainer = styled.div`
   flex-direction: column;
   justify-content: space-between;
 `;
-export const TypeDisplay = styled.div`
-  grid-row: 3;
-  grid-column: 1;
-  display: flex;
-  flex-direction: row;
-  align-self: center;
-  justify-self: end;
-  height: 100%;
-`;
-
-export const Type = styled(TypeColoredComponent)`
-  margin: 0 5px;
-  padding: 1px 15px;
-  border-radius: 20px;
-  display: flex;
-  align-items: center;
-`;
-export const StatsDisplay = styled.div`
-  grid-row: 2/4;
-  grid-column: 2/4;
-  width: 250px;
-  display: flex;
-  justify-self: end;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  /* border: 3px solid black; */
-  flex-wrap: wrap;
-  font-size: 1.2em;
-  height: 70px;
-`;
-export const StatBox = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  width: 60px;
-`;
-export const StatName = styled.div``;
-export const StatValue = styled.div``;
