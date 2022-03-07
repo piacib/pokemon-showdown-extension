e0226b1266310bd34cce28fd2d4d8d47745f1b3d
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-03-06 19:37:10 -1000

    updated AbilitiesDisplay

--- src/components/PokemonDataDisplay/OtherFormatsDisplay.tsx
+++ src/components/PokemonDataDisplay/OtherFormatsDisplay.tsx
@@ -1,26 +1,13 @@
 import { Dex } from '@pkmn/dex';
-import { dexSearchPrepper } from '../../functions';
-import { AbilitiesDisplay, HiddenPropertyText, PropertyBtn } from './DataDisplay.styles';
+import AbilitiesDisplay from '../AbilitiesDisplay/AbilitiesDisplay';
 
 interface OtherFormatsDisplayProps {
   pokemon: string;
 }
 
-export const OtherFormatsDisplay = ({ pokemon }: OtherFormatsDisplayProps) => {
+const OtherFormatsDisplay = ({ pokemon }: OtherFormatsDisplayProps) => {
   const abilities = Object.entries(Dex.species.get(pokemon).abilities).map((x) => x[1]);
-  return (
-    <AbilitiesDisplay>
-      Abilities:
-      {abilities.map((x) => (
-        <>
-          <PropertyBtn>
-            {x}
-            <HiddenPropertyText>
-              {Dex.data.Abilities[dexSearchPrepper(x)].shortDesc}
-            </HiddenPropertyText>
-          </PropertyBtn>
-        </>
-      ))}
-    </AbilitiesDisplay>
-  );
+  return <AbilitiesDisplay abilities={abilities} />;
 };
+
+export default OtherFormatsDisplay;
